85115cca8eb7f4ec27070c6198a9dd37e6afd0a3
alanjaskot <alanjaskot@gmail.com>
2023-04-30 08:40:51 +0200

    feat. add logic for shared button

--- src/app/shared/components/ui-button/ui-button.component.ts
+++ src/app/shared/components/ui-button/ui-button.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-ui-button',
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./ui-button.component.css']
 })
 export class UiButtonComponent {
-
+  @Input() text = 'button';
+  @Input() color: 'primary' | 'warn' | 'accent' = 'primary';
+  @Input() disabled = false;
+  @Input() buttonType: 'raised' | 'mat' | 'fab' | 'icon-button' = 'raised';
+  @Input() icon?: string;
+  @Input() iconColor?: string;  
 }
