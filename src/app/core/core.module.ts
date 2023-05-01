d3bd19c502f868897b332a39b02889c48495870b
alanjaskot <alanjaskot@gmail.com>
2023-05-01 19:17:07 +0200

    feat. add navbar component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -8,11 +8,12 @@ import { SharedModule } from '../shared/shared.module';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { AuthUserService } from './services/auth-user.service';
 import { RegisterComponent } from './components/register/register.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
-  declarations: [DashboardComponent, LoginComponent, RegisterComponent],
+  declarations: [DashboardComponent, LoginComponent, NavbarComponent, RegisterComponent],
   imports: [HttpClientModule, MaterialModule, ReactiveFormsModule, SharedModule],
   providers: [AuthUserService],
-  exports: [DashboardComponent, LoginComponent],
+  exports: [DashboardComponent, LoginComponent, NavbarComponent],
 })
 export class CoreModule {}
