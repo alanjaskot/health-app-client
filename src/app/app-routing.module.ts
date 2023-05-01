c9a81bd8c265bf0adbf8ebd814a54ecf8b45515e
alanjaskot <alanjaskot@gmail.com>
2023-05-01 07:53:29 +0200

    feat. add register component to routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './core/components/login/login.component';
 import { DashboardComponent } from './core/components/dashboard/dashboard.component';
+import { RegisterComponent } from './core/components/register/register.component';
 
 const routes: Routes = [
   { path: '', component: DashboardComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
   { path: '**', component: DashboardComponent },
 ];
 
