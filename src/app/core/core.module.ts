5dea517b2cfd0f26aec4ea995cf61bbe2163927b
alanjaskot <alanjaskot@gmail.com>
2023-05-01 07:52:39 +0200

    feat. add register component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -7,9 +7,10 @@ import { LoginComponent } from './components/login/login.component';
 import { SharedModule } from '../shared/shared.module';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { AuthUserService } from './services/auth-user.service';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
-  declarations: [DashboardComponent, LoginComponent],
+  declarations: [DashboardComponent, LoginComponent, RegisterComponent],
   imports: [HttpClientModule, MaterialModule, ReactiveFormsModule, SharedModule],
   providers: [AuthUserService],
   exports: [DashboardComponent, LoginComponent],
