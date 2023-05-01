2b452fd93360c28d84f6a7a32988ada7be597ab0
alanjaskot <alanjaskot@gmail.com>
2023-05-01 18:25:46 +0200

    feat. add logic for register component

--- src/app/core/components/register/register.component.ts
+++ src/app/core/components/register/register.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { RegisterForm } from '../../forms/register.form';
+import { AuthUserService } from '../../services/auth-user.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
+  form: RegisterForm = new RegisterForm();
 
+  constructor(private userService: AuthUserService, private toastr: ToastrService) {}
+
+  registerUser(): void {
+    if (!this.form.valid) {
+      return;
+    }
+
+    if (this.form.password.value !== this.form.confirmPassword.value) {
+      this.toastr.error('passwords are not the same');
+    }
+
+    this.userService.register(this.form.userName.value, this.form.password.value);
+  }
 }
