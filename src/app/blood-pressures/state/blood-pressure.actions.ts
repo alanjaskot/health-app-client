c6f0f89473386b2da1ba03aa3aa251aee45c708f
alanjaskot <alanjaskot@gmail.com>
2023-05-05 21:19:47 +0200

    feat. add actions for blood pressure state

---
+++ src/app/blood-pressures/state/blood-pressure.actions.ts
@@ -0,0 +1,31 @@
+import { IBloodPressure } from '../models/blood-pressure.model';
+
+export class LoadBloodPressures {
+  static type = '[Blood Pressure] Fetch Blood Pressures';
+  constructor() {}
+}
+
+export class LoadDeletedBloodPressures {
+  static type = '[Blood Pressure] Fetch Deleted Blood Pressures';
+  constructor(public userId: string, public isDeleted: boolean) {}
+}
+
+export class LoadBloodPressuresById {
+  static type = '[Blood Pressure] Get Blood Pressure By Id';
+  constructor(public id: string) {}
+}
+
+export class AddBloodPressure {
+  static type = '[Blood Pressure] Add Blood Pressure';
+  constructor(public toCreate: IBloodPressure) {}
+}
+
+export class UpdateBloodPressure {
+  static type = '[Blood Pressure] Update Blood Pressure';
+  constructor(public toUpdate: IBloodPressure) {}
+}
+
+export class DeleteBloodPressure {
+  static type = '[Blood Pressure] Delete Blood Pressure';
+  constructor(public id: string) {}
+}
