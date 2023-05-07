1fe116e3f70d4c1b456c1a18285327ad8fd78d2e
alanjaskot <alanjaskot@gmail.com>
2023-05-07 15:52:51 +0200

    feat. add logic and html for blood pressure list

--- src/app/blood-pressures/components/user/list-blood-pressure/list-blood-pressure.component.ts
+++ src/app/blood-pressures/components/user/list-blood-pressure/list-blood-pressure.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Select } from '@ngxs/store';
 import { Observable } from 'rxjs';
 import { IBloodPressure } from 'src/app/blood-pressures/models/blood-pressure.model';
+import { BloodPressureService } from 'src/app/blood-pressures/services/blood-pressure-api.service';
 import { BloodPressureState } from 'src/app/blood-pressures/state/blood-pressure.state';
 
 @Component({
@@ -9,9 +10,16 @@ import { BloodPressureState } from 'src/app/blood-pressures/state/blood-pressure
   templateUrl: './list-blood-pressure.component.html',
   styleUrls: ['./list-blood-pressure.component.css'],
 })
-export class ListBloodPressureComponent {
+export class ListBloodPressureComponent implements OnInit {
   @Select(BloodPressureState.bloodPressuresFetched)
   fetchBloodPressures$: Observable<IBloodPressure>[];
+  data$: Observable<IBloodPressure[]>;
 
   columnsToDisplay = ['id', 'diastolic pressure', 'systolic pressures', 'pulse'];
+
+  constructor(private service: BloodPressureService) {}
+
+  ngOnInit(): void {
+    this.data$ = this.service.getAllBloodPressures();
+  }
 }
