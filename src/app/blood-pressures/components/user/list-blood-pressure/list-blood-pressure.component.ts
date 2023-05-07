4547e5820cdc600130a32baa4359002d5535a0ff
alanjaskot <alanjaskot@gmail.com>
2023-05-07 12:22:19 +0200

    feat. add temp blood pressure list

--- src/app/blood-pressures/components/user/list-blood-pressure/list-blood-pressure.component.ts
+++ src/app/blood-pressures/components/user/list-blood-pressure/list-blood-pressure.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Select } from '@ngxs/store';
+import { Observable } from 'rxjs';
+import { IBloodPressure } from 'src/app/blood-pressures/models/blood-pressure.model';
+import { BloodPressureState } from 'src/app/blood-pressures/state/blood-pressure.state';
 
 @Component({
   selector: 'app-list-blood-pressure',
   templateUrl: './list-blood-pressure.component.html',
-  styleUrls: ['./list-blood-pressure.component.css']
+  styleUrls: ['./list-blood-pressure.component.css'],
 })
 export class ListBloodPressureComponent {
+  @Select(BloodPressureState.bloodPressuresFetched)
+  fetchBloodPressures$: Observable<IBloodPressure>[];
 
+  columnsToDisplay = ['id', 'diastolic pressure', 'systolic pressures', 'pulse'];
 }
