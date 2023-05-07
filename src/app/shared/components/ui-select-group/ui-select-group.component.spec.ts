55e328013a19549dcfb9f624478500cc8592328d
alanjaskot <alanjaskot@gmail.com>
2023-05-07 16:51:01 +0200

    feat. add ui group select

---
+++ src/app/shared/components/ui-select-group/ui-select-group.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UiSelectGroupComponent } from './ui-select-group.component';
+
+describe('UiSelectGroupComponent', () => {
+  let component: UiSelectGroupComponent;
+  let fixture: ComponentFixture<UiSelectGroupComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ UiSelectGroupComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(UiSelectGroupComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/shared/components/ui-select-group/ui-select-group.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ui-select-group',
+  templateUrl: './ui-select-group.component.html',
+  styleUrls: ['./ui-select-group.component.css']
+})
+export class UiSelectGroupComponent {
+
+}
