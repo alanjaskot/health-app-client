5cdba2b542541b099de145e6f602f92e677dfda8
alanjaskot <alanjaskot@gmail.com>
2023-05-07 18:08:43 +0200

    feat. add logic and view of datepicker

--- src/app/shared/components/ui-datepicker/ui-datepicker.component.ts
+++ src/app/shared/components/ui-datepicker/ui-datepicker.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { UntypedFormControl } from '@angular/forms';
+import { MatDatepickerInputEvent } from '@angular/material/datepicker';
 
 @Component({
   selector: 'app-ui-datepicker',
   templateUrl: './ui-datepicker.component.html',
-  styleUrls: ['./ui-datepicker.component.css']
+  styleUrls: ['./ui-datepicker.component.css'],
 })
 export class UiDatepickerComponent {
+  @Input() label = '';
+  @Input() control: UntypedFormControl;
+  @Input() placeholder?: string;
+  @Input() required = true;
+  @Input() selected: boolean;
+  @Input() min: Date;
 
+  @Output() readonly changeHandler: EventEmitter<MatDatepickerInputEvent<Date>> = new EventEmitter<
+    MatDatepickerInputEvent<Date>
+  >();
+
+  onChange(event: MatDatepickerInputEvent<Date>): void {
+    this.changeHandler.emit(event);
+  }
 }
