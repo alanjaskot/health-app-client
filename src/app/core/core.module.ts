5d72275ceaf1ef440035b478091e6b440941c57d
alanjaskot <alanjaskot@gmail.com>
2023-05-21 19:55:11 +0200

    feat. add left panel component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -11,11 +11,18 @@ import { AuthUserService } from './services/auth-user.service';
 import { RegisterComponent } from './components/register/register.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { AuthService } from './services/auth.service';
+import { MainMenuComponent } from './components/main-menu/main-menu.component';
 
 @NgModule({
-  declarations: [DashboardComponent, LoginComponent, NavbarComponent, RegisterComponent],
+  declarations: [
+    DashboardComponent,
+    LoginComponent,
+    MainMenuComponent,
+    NavbarComponent,
+    RegisterComponent,
+  ],
   imports: [CommonModule, HttpClientModule, MaterialModule, ReactiveFormsModule, SharedModule],
   providers: [AuthService, AuthUserService],
-  exports: [DashboardComponent, LoginComponent, NavbarComponent],
+  exports: [DashboardComponent, LoginComponent, MainMenuComponent, NavbarComponent],
 })
 export class CoreModule {}
