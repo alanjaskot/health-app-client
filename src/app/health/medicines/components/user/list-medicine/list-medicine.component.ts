683d43063e5c494e2cc4dbe73a8a53683185f481
alanjaskot <alanjaskot@gmail.com>
2023-11-05 19:19:26 +0100

    feat. change table to shared in medicine

--- src/app/health/medicines/components/user/list-medicine/list-medicine.component.ts
+++ src/app/health/medicines/components/user/list-medicine/list-medicine.component.ts
@@ -1,26 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { MedicineApiService } from '../../../services/medicine-api.service';
+import { Select, Store } from '@ngxs/store';
+import { Observable } from 'rxjs';
+import { MedicineState } from '../../../state/medicine.state';
+import { LoadMedicines } from '../../../state/medicine.actions';
+import { IMedicineModel } from '../../../models/medicine.model';
 
 @Component({
   selector: 'app-list-medicine',
   templateUrl: './list-medicine.component.html',
   styleUrls: ['./list-medicine.component.scss'],
 })
-export class ListMedicineComponent {
-  displayingColumns: string[] = [
-    'id',
+export class ListMedicineComponent implements OnInit{
+  @Select(MedicineState.medicinesFetched) data$: Observable<IMedicineModel[]>;
+  displayedColumns: string[] = [
     'name',
     'dose',
     'unit',
-    'medicalDosage',
-    'created',
-    'updated',
+    'medical dosage'
   ];
+  url: string = '/medicine';
 
-  constructor(private service: MedicineApiService, private router: Router) {}
-
-  getAllMedicines$ = this.service.getAllMedicines();
+  constructor(private router: Router, private store: Store) {}
+  
+  ngOnInit(): void {
+    this.store.dispatch(new LoadMedicines)
+  }
 
   routeTo(): void {
     this.router.navigate(['/medicine/new']);
