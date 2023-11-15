a38e4caba9979ae542252ef978cf463826ed2b34
alanjaskot <alanjaskot@gmail.com>
2023-11-15 18:53:58 +0100

    fix. app test file

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,18 +16,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'health-app-client'`, () => {
+  it(`should have as title 'Home App'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('health-app-client');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'health-app-client app is running!'
-    );
+    expect(app.title).toEqual('Home App');
   });
 });
